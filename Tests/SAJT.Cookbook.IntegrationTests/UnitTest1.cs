55ed9129b655fc08c8e56d5d5416d813a2482990
simon <simonagmell@gmail.com>
2025-10-05 21:51:03 +0200

    Add test folders

---
+++ Tests/SAJT.Cookbook.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace SAJT.Cookbook.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Tests/SAJT.Cookbook.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace SAJT.Cookbook.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
